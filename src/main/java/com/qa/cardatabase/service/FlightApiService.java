688f216b93f24604bee1071518ea80d3e965605d
SteveReade182 <sjreade13@outlook.com>
2023-02-21 22:47:19 +0000

    changed request to departureDate

--- src/main/java/com/qa/cardatabase/service/FlightApiService.java
+++ src/main/java/com/qa/cardatabase/service/FlightApiService.java
@@ -17,7 +17,7 @@ public class FlightApiService {
         RestTemplate restTemplate = new RestTemplate();
         String API_Key = "access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiOWE5YjFhODk0NDczMmZlODg0MzA4OGIxMTBmOTE1ZGFlZTlkMjUwNzIyMjE3NjkwMTZjNThlNjAyYzI4MTc0ZjMxODQ2YzFmNjI4ZTMwNzUiLCJpYXQiOjE2NzY1ODQwOTQsIm5iZiI6MTY3NjU4NDA5NCwiZXhwIjoxNzA4MTIwMDk0LCJzdWIiOiIyMDA1MCIsInNjb3BlcyI6W119.mlQ3VFAMWes2TFiPGzgCl_tQe3khTpgCO49oh9eigNVmzVd3Cw0S4oS9l8WaOMyDYhYXo3DhHnla_qybx23DDw"; //API KEY
         String departureDate = "2023-02-25"; // example date
-        String query = "&adults=1&origin=LHR&destination=KEF&date=" + departureDate;
+        String query = "&adults=1&origin=LHR&destination=KEF&departureDate=" + departureDate;
         String apiUrl = API_URL + "?" +API_Key + query;
         ResponseEntity<Flight[]> response = restTemplate.getForEntity(apiUrl, Flight[].class);
         return Arrays.asList(response.getBody());
