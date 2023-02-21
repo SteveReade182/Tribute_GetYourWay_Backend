89651f41f6958e33966bcf73dccfb23b8e468e04
SteveReade182 <sjreade13@outlook.com>
2023-02-21 16:15:23 +0000

    changed app properties for our front end

--- src/main/java/com/qa/cardatabase/SecurityConfig.java
+++ src/main/java/com/qa/cardatabase/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 	http.csrf().disable().cors().and().authorizeRequests()
 	.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.antMatchers(HttpMethod.POST, "/register").permitAll()
 	.anyRequest().authenticated()
 	.and()
 	// Filter for the api/login requests
