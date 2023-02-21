5848d5108243075d7707c4baf51e15bcf86006e4
SteveReade182 <sjreade13@outlook.com>
2023-02-21 16:57:12 +0000

    added Flight service and API_URL

---
+++ src/main/java/com/qa/cardatabase/service/FlightApiService.java
@@ -0,0 +1,34 @@
+package com.qa.cardatabase.service;
+
+import com.qa.cardatabase.data.entity.Flight;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Component
+public class FlightApiService {
+    private static final String API_URL = "https://app.goflightlabs.com/search-all-flights";
+
+    public List<Flight> getOutboundFlights() {
+        RestTemplate restTemplate = new RestTemplate();
+        String API_Key = "access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiOWE5YjFhODk0NDczMmZlODg0MzA4OGIxMTBmOTE1ZGFlZTlkMjUwNzIyMjE3NjkwMTZjNThlNjAyYzI4MTc0ZjMxODQ2YzFmNjI4ZTMwNzUiLCJpYXQiOjE2NzY1ODQwOTQsIm5iZiI6MTY3NjU4NDA5NCwiZXhwIjoxNzA4MTIwMDk0LCJzdWIiOiIyMDA1MCIsInNjb3BlcyI6W119.mlQ3VFAMWes2TFiPGzgCl_tQe3khTpgCO49oh9eigNVmzVd3Cw0S4oS9l8WaOMyDYhYXo3DhHnla_qybx23DDw"; //API KEY
+        String departureDate = "2023-02-25"; // example date
+        String query = "&adults=1&origin=LHR&destination=KEF&date=" + departureDate;
+        String apiUrl = API_URL + "?" +API_Key + query;
+        ResponseEntity<Flight[]> response = restTemplate.getForEntity(apiUrl, Flight[].class);
+        return Arrays.asList(response.getBody());
+    }
+}
+
+
+//https://app.goflightlabs.com/search-all-flights
+// access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiOWE5YjFhODk0NDczMmZlODg0MzA4OGIxMTBmOTE1ZGFlZTlkMjUwNzIyMjE3NjkwMTZjNThlNjAyYzI4MTc0ZjMxODQ2YzFmNjI4ZTMwNzUiLCJpYXQiOjE2NzY1ODQwOTQsIm5iZiI6MTY3NjU4NDA5NCwiZXhwIjoxNzA4MTIwMDk0LCJzdWIiOiIyMDA1MCIsInNjb3BlcyI6W119.mlQ3VFAMWes2TFiPGzgCl_tQe3khTpgCO49oh9eigNVmzVd3Cw0S4oS9l8WaOMyDYhYXo3DhHnla_qybx23DDw
+// &adults=1
+// &origin=LHR
+// &destination=KEF
+// &departureDate=2023-02-24
+
