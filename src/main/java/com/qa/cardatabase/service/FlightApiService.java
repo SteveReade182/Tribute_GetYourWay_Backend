7c6c698f44d571d4602a319640e15381af84f0bd
SteveReade182 <sjreade13@outlook.com>
2023-02-22 10:24:59 +0000

    flight api call working - needs logic and setters for class

--- src/main/java/com/qa/cardatabase/service/FlightApiService.java
+++ src/main/java/com/qa/cardatabase/service/FlightApiService.java
@@ -11,17 +11,34 @@ import java.util.List;
 
 @Component
 public class FlightApiService {
-    private static final String API_URL = "https://app.goflightlabs.com/search-all-flights";
+    private static final String API_URL = "https://airlabs.co/api/v9/schedules";
 
-    public List<Flight> getOutboundFlights() {
+//    public List<Flight> getOutboundFlights() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String API_Key = "api key = 135f1a6f-5adb-43af-bee8-2304b4fddcd6"; //API KEY
+////        String departureDate = "2023-02-25"; // example date
+//        String query = "dep_iata=LHR&arr_iata=KEF&";// + departureDate;
+//        String apiUrl = API_URL + "?" +query+API_Key;
+//        ResponseEntity<Flight[]> response = restTemplate.getForEntity(apiUrl, Flight[].class);
+//        return Arrays.asList(response.getBody());
+//    }
+
+    public String getSchedules(){
         RestTemplate restTemplate = new RestTemplate();
-        String API_Key = "access_key=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiOWE5YjFhODk0NDczMmZlODg0MzA4OGIxMTBmOTE1ZGFlZTlkMjUwNzIyMjE3NjkwMTZjNThlNjAyYzI4MTc0ZjMxODQ2YzFmNjI4ZTMwNzUiLCJpYXQiOjE2NzY1ODQwOTQsIm5iZiI6MTY3NjU4NDA5NCwiZXhwIjoxNzA4MTIwMDk0LCJzdWIiOiIyMDA1MCIsInNjb3BlcyI6W119.mlQ3VFAMWes2TFiPGzgCl_tQe3khTpgCO49oh9eigNVmzVd3Cw0S4oS9l8WaOMyDYhYXo3DhHnla_qybx23DDw"; //API KEY
-        String departureDate = "2023-02-25"; // example date
-        String query = "&adults=1&origin=LHR&destination=KEF&departureDate=" + departureDate;
-        String apiUrl = API_URL + "?" +API_Key + query;
-        ResponseEntity<Flight[]> response = restTemplate.getForEntity(apiUrl, Flight[].class);
-        return Arrays.asList(response.getBody());
+        String API_Key = "api_key=135f1a6f-5adb-43af-bee8-2304b4fddcd6"; // API KEY
+        String departureIATA = "LHR";
+        String destinationIATA = "KEF";
+
+        String url = API_URL + "?" + "dep_iata=" + departureIATA + "&arr_iata=" + destinationIATA + "&" + API_Key;
+
+        String json = restTemplate.getForObject(url, String.class);
+
+        //logic to then set the Flight class variables
+        return json;
     }
+
+
+
 }
 
 
@@ -32,3 +49,22 @@ public class FlightApiService {
 // &destination=KEF
 // &departureDate=2023-02-24
 
+
+//api key = 135f1a6f-5adb-43af-bee8-2304b4fddcd6
+
+//https://airlabs.co/api/v9/schedules?dep_iata=LHR&arr_iata=KEF&api_key=135f1a6f-5adb-43af-bee8-2304b4fddcd6
+
+
+//original below
+
+//    private static final String API_URL = "https://airlabs.co/api/v9/schedules";
+//
+//    public List<Flight> getOutboundFlights() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String API_Key = "api key = 135f1a6f-5adb-43af-bee8-2304b4fddcd6"; //API KEY
+////        String departureDate = "2023-02-25"; // example date
+//        String query = "dep_iata=LHR&arr_iata=KEF&";// + departureDate;
+//        String apiUrl = API_URL + "?" +query+API_Key;
+//        ResponseEntity<Flight[]> response = restTemplate.getForEntity(apiUrl, Flight[].class);
+//        return Arrays.asList(response.getBody());
+//    }
