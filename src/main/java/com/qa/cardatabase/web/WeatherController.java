169699fac7ea20446a65e731e19d080ed603cd33
arumugac <chithra_thasan@yahoo.co.uk>
2023-02-22 15:37:23 +0000

    created backend weather data API

---
+++ src/main/java/com/qa/cardatabase/web/WeatherController.java
@@ -0,0 +1,27 @@
+package com.qa.cardatabase.web;
+
+import com.qa.cardatabase.data.entity.Weather;
+import com.qa.cardatabase.service.WeatherResponseService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class WeatherController {
+    private WeatherResponseService service;
+    public WeatherController(WeatherResponseService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/weather/{location}")
+    public Weather weatherRequest(@PathVariable String location) {
+
+        return this.service.getWeather(location);
+    }
+    @GetMapping("/weather")
+    public Weather weatherRequestByCityName(@RequestParam String cityName) {
+        return this.service.getWeather(cityName);
+    }
+}
